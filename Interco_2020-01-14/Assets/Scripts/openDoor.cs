3f059a1b0e3e0b21da25d71843ec8746af8b03a6
Xavier Jeanson <xavier_jeanson@hotmail.ca>
2020-01-15 21:40:05 -0500

    3

--- Interco_2020-01-14/Assets/Scripts/openDoor.cs
+++ Interco_2020-01-14/Assets/Scripts/openDoor.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class openDoor : MonoBehaviour
 {
+    public GameObject caveLight;
     public GameObject door;
     // Start is called before the first frame update
     void Start()
@@ -20,6 +21,19 @@ public class openDoor : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Debug.Log("pouf");
-        
+        Destroy(door);
+        foreach(GameObject go in GameObject.FindGameObjectsWithTag("worldLight"))
+        {
+            go.SetActive(false);
+        }
+        GameObject g = GameObject.FindGameObjectWithTag("caveLight");
+        if (g != null) {
+            Debug.Log("find");
+            g.SetActive(true);
+        }
+        else
+        {
+            caveLight.SetActive(true);
+        }
     }
 }
