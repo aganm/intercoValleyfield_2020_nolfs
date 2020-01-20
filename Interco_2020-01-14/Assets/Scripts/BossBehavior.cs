bd38f25dcf5c0c8930863bfb3e0f393fcd13e500
Michael Aganier <michaelaganier@gmail.com>
2020-01-20 15:34:42 -0500

    Center Boss on self box collider

--- Interco_2020-01-14/Assets/Scripts/BossBehavior.cs
+++ Interco_2020-01-14/Assets/Scripts/BossBehavior.cs
@@ -5,17 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class BossBehavior : MonoBehaviour
 {
-        public float YOffset = 0;
+        private GameObject camera = null;
+        private BoxCollider2D collider = null;
+
         // Start is called before the first frame update
         void Start()
         {
-
+                camera = GameObject.FindGameObjectWithTag("MainCamera");
+                collider = GetComponent<BoxCollider2D>();
         }
 
         // Update is called once per frame
         void Update()
         {
-                transform.position = new Vector3(transform.position.x, GameObject.FindGameObjectWithTag("MainCamera").transform.position.y + YOffset);
+                transform.position = new Vector3(transform.position.x, camera.transform.position.y - collider.bounds.extents.y / 2f);
         }
 
         private void OnTriggerEnter2D(Collider2D collision)
