316a4337567494b1d96bc5729e0f167893686f61
Michael Aganier <michaelaganier@gmail.com>
2020-01-21 01:19:21 -0500

    Work on level 4

---
+++ Interco_2020-01-14/Assets/CameraPlayerSpeed.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPlayerSpeed : MonoBehaviour
+{
+        private float defaultSize;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+                defaultSize = GetComponent<Camera>().orthographicSize;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+                float speed = Mathf.Abs(GameObject.FindGameObjectWithTag("Player").GetComponent<Rigidbody2D>().velocity.magnitude);
+                float newSize = defaultSize * (speed * 0.1f + 1f);
+                GetComponent<Camera>().orthographicSize = newSize;
+        }
+}
